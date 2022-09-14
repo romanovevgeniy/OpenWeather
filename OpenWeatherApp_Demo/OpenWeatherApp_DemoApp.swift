aa46900b2197b3a648150b24ebaa9b3a1eb3744f
eromanov <romanov.evgeniy86@gmail.com>
2022-09-14 17:56:33 +0300

    Initial Commit

---
+++ OpenWeatherApp_Demo/OpenWeatherApp_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp_DemoApp.swift
+//  OpenWeatherApp_Demo
+//
+//  Created by Евгений Романов on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
