ef95be5b726546948568e0fa9fa1b14fd3480065
eromanov <romanov.evgeniy86@gmail.com>
2022-09-15 22:55:03 +0300

    fix

--- OpenWeatherApp_Demo/Managers/WeatherManager.swift
+++ OpenWeatherApp_Demo/Managers/WeatherManager.swift
@@ -10,7 +10,7 @@ import CoreLocation
 
 class WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
-        guard let url = URL(string: "https://api.openweathermap.org/data/3.0/onecall?lat=\(latitude)&lon=\(longitude)&appid=\("0b2f85bd58539fa52cf02349e126336d")&units=metric") else {
+        guard let url = URL(string: "https://api.openweathermap.org/data/3.0/onecall?lat=\(latitude)&lon=\(longitude)&appid=0b2f85bd58539fa52cf02349e126336d&units=metric") else {
             fatalError("Отсутствует URL адрес")
             
         }
@@ -23,61 +23,38 @@ class WeatherManager {
 }
 
 struct ResponseBody: Decodable {
-    let coord: CoordinatesResponse
-    let weather: [WeatherResponse]
-    let base: String
-    let main: Main
-    let visibility: Int
-    let wind: Wind
-    let clouds: Clouds
-    let dt: Int
-    let sys: Sys
-    let timezone, id: Int
-    let name: String
-    let cod: Int
-}
-
-struct Clouds: Codable {
-    let all: Int
-}
-
-struct CoordinatesResponse: Decodable {
-    let lon, lat: Double
-}
-
-struct Main: Codable {
-    let temp, feelsLike, tempMin, tempMax: Double
-    let pressure, humidity, seaLevel, grndLevel: Int
-
-    enum CodingKeys: String, CodingKey {
-        case temp
-        case feelsLike = "feels_like"
-        case tempMin = "temp_min"
-        case tempMax = "temp_max"
-        case pressure, humidity
-        case seaLevel = "sea_level"
-        case grndLevel = "grnd_level"
+    var coord: CoordinatesResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var wind: WindResponse
+    var name: String
+    
+    struct CoordinatesResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    }
+    
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+    
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feelsLike: Double
+        var tempMin: Double
+        var tempMax: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
     }
 }
 
-struct Sys: Codable {
-    let country: String
-    let sunrise, sunset: Int
-}
 
-struct WeatherResponse: Decodable {
-    let id: Int
-    let main, weatherDescription, icon: String
 
-    enum CodingKeys: String, CodingKey {
-        case id, main
-        case weatherDescription = "description"
-        case icon
-    }
-}
-
-struct Wind: Codable {
-    let speed: Double
-    let deg: Int
-    let gust: Double
-}
